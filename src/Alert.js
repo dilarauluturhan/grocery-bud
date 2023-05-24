793844d2d174d89abfb926eca111bf4a2e56df6a
Dilara Uluturhan <dilarauluturhan@outlook.com>
2023-05-24 20:19:46 +0300

    Grocery Bud project setup

---
+++ src/Alert.js
@@ -0,0 +1,9 @@
+import React, {useEffect} from 'react'
+
+const Alert = () => {
+  return (
+    <div>Alert Component</div>
+  )
+}
+
+export default Alert
---
+++ src/List.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { FaEdit, FaTrash } from 'react-icons/fa';
+
+const List = () => {
+  return (
+    <div>List Component</div>
+  )
+}
+
+export default List
