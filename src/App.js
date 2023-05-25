331fdb7af1511ffc4416732d5bde76ad5cb92a4f
Dilara Uluturhan <dilarauluturhan@outlook.com>
2023-05-25 22:35:48 +0300

    Bud structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import React,{useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import List from './List.js';
 import Alert from './Alert.js';
 
 function App() {
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div>Grocery Bud Setup</div>
+    <section className='section-center'>
+      <form className='grocery-form' onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>Grocery Bud</h3>
+        <div className='form-control'>
+          <input 
+          type='text' 
+          className='grocery' 
+          placeholder='e.g. eggs' 
+          value={name} 
+          onChange={(e) => setName(e.target.value)}
+          />
+          <button type='submit' className='submit-btn'>
+            {isEditing ? 'edit' : 'submit'}
+          </button>
+        </div>
+      </form>
+      <div className='grocery-container'>
+        <List />
+        <button className='clear-btn'>Clear Items</button>
+      </div>
+    </section>
   )
 }
 
